95a45233d63a8916f7fd2d2467d76240f0556a35
Vinicius Brizolla <vinibrizolla@gmail.com>
2025-09-26 16:34:36 -0300

    adicionado um paragrafo

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home() {
-return <h1>Teste 3</h1>
+return (
+  <>
+     <h1>Test</h1>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, architecto a non unde pariatur rem ab perferendis quo illo voluptates ipsa labore modi dolorem, earum doloribus maxime voluptatem nam molestias?</p>
+  </>
+)
+
 }
 
 export default Home; 
