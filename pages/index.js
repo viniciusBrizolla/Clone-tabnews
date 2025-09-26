fbcfcedebde1f26ccf68fba2e6cd1bf078da9ca5
Vinicius Brizolla <vinibrizolla@gmail.com>
2025-09-26 17:22:59 -0300

    atualização de titulo e adição de nomes

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
 function Home() {
-return (
-  <>
-     <h1>Test</h1>
-     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, architecto a non unde pariatur rem ab perferendis quo illo voluptates ipsa labore modi dolorem, earum doloribus maxime voluptatem nam molestias?</p>
-  </>
-)
-
+  return (
+    <>
+      <h1>Test 02</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, architecto
+        a non unde pariatur rem ab perferendis quo illo voluptates ipsa labore
+        modi dolorem, earum doloribus maxime voluptatem nam molestias?
+      </p>
+      <p>Vinicius </p>
+      <p>Leonardo</p>
+    </>
+  );
 }
 
-export default Home; 
+export default Home;
